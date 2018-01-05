ef418a16f7ae639b3aa281c0a70802f76fa98b49
syk531 <syk531@naver.com>
2018-01-05 10:53:59 +0900

    heroku test

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.get('/', function(req, res) {
 	res.sendfile('./front/index.html');
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
 	console.log('server starting');
 })
